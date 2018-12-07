13e082b5d399f98397f9c7945adf7be4db6bee3e
Richard Mayes <caveman.dick@gmail.com>
2018-12-07 14:36:44 +0000

    Add day 2 pt 1 sln

--- day-two/python/part-one.py
+++ day-two/python/part-one.py
@@ -1,6 +1,29 @@
-file = open('../input.txt', 'r')
+file = open('input.txt', 'r')
 answer = 0
 
+twoLetterCount = 0
+threeLetterCount = 0
+
 for line in file:
-    print(f'{line}')
+    twoLetter = False
+    threeLetter = False
+
+    for letter in line:
+        letterCount = line.count(letter)
+        if letterCount == 2:
+            twoLetter = True
+
+        if letterCount == 3:
+            threeLetter = True
+
+        if twoLetter & threeLetter:
+            break
+
+    if twoLetter:
+        twoLetterCount += 1
+    if threeLetter:
+        threeLetterCount += 1
+
+
+print(f'Checksum = {twoLetterCount * threeLetterCount}')
 
